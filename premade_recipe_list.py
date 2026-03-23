a399a7ab0ae4f42700c118982903750eda34ee5f
floran-yaadar <feky2@kent.ac.uk>
2026-03-23 11:35:59 +0000

    imade an introductory interface for my recipe scaler and im starting to create premade recipes in a seperate file which i plan to callin my main programme. so far ive made a recipe for pancakes

---
+++ premade_recipe_list.py
@@ -0,0 +1,42 @@
+import cowsay
+def pancakes():
+    ingredients = ["flour", "baking powder", "sugar", "salt", "milk", "butter", "egg"]
+    measurments = ["1.5", "3.5", "1", "0.25", "1.25", "3", "1"]
+    scale = ["cups", "teaspoons", "tablespoons", "teaspoons", "cups", "tablespoons", ""]
+    original_servings = 8
+    cowsay.fox("how many servings would you like to make?")
+    servings = int(input())
+    measuremnts = [float(i)/original_servings for i in measurments]
+    new_measurments = [i * servings for i in measuremnts]
+    cowsay.fox(f"here are the ingredients for panckes\n {servings} servings ")
+    for i in range(len(ingredients)):
+        print (f"{ingredients[i]} {new_measurments[i]} {scale[i]} ")
+        continue_recipe()
+        end()
+def continue_recipe():
+    cowsay.fox("Would you liketo to make this? ")
+    continue_answer = input("[Y]es or [N]o").lower().strip()
+    if continue_answer == "y":
+        return pancakes_instructions()
+    elif continue_answer == "n":
+        cowsay.fox("okay, here are some of our other recipes!")
+        return recipe_menu()
+    else:
+        cowsay.fox("please enter a valid option")
+        return continue_recipe()
+def pancakes_instructions():
+    print("1.Sift flour, baking powder, sugar, and salt together in a large bowl. Make a well in the center and add milk, melted butter, and egg; mix until smooth.\n2. Heat a lightly oiled griddle or pan over medium-high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake; cook until bubbles form and the edges are dry, about 2 to 3 minutes.\n3. Flip and cook until browned on the other side. Repeat with remaining batter.")
+    cowsay.fox("serve and ENJOY!!")
+def end():
+    cowsay.fox("would you like to make another recipe?")
+    end_answer = input("[Y]es or [N]o").lower().strip()
+    if end_answer == "y":
+        return open_menu()
+    elif end_answer == "n":
+        cowsay.fox("okay have a nice day! ")
+    else:
+        cowsay.fox("please enter a valid option")
+        return end()
+    
+pancakes()
+    
---
+++ test.py
@@ -0,0 +1,34 @@
+import cowsay
+import re
+def main():
+    open_menu()
+def validate_email(email):
+    email = input("please enter your email: ")
+    if re.search(r".+@.+\.(ac.uk | com)$", email):
+        return print (cowsay.fox("Email address is valid"))
+    else: 
+        return print (cowsay.fox("Sorry, that email address is not valid, please\n try again"))
+
+def open_menu():
+    cowsay.fox("Welcome to the recipe scaler")
+    cowsay.fox(" Would u like a premade recipe or would you like to create your own?")
+    answer = input("[A] premade.  [B] create your own").lower().strip()
+    if answer == "a":
+        return recipe_menu()
+    elif answer == "b":
+        return create_recipe()
+    else:
+        return cowsay.fox("Sorry that isn't a valid option, please try again.")
+    open_menu()
+    
+def recipe_menu():
+    cowsay.fox("Here are the premade recipes")
+    recipe_select = input("1. Pancakes\n2. spaghetti bolognese\n3. garlic butter chicken")
+    if recipe_select == "1":
+        return pancakes()
+
+    
+def create_recipe():
+    pass
+
+main()
